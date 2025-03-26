dfaedaed4a53ef2e9f746e5075566a5e3981ae18
ZermbaGerd <uuser7821@gmail.com>
2025-03-26 12:40:42 -0500

    added some basic tiles to lobbies and some stylesheets for index/lobbies

--- static/lobby.js
+++ static/lobby.js
@@ -1,17 +1,25 @@
 var socket = io();
 var lobby = window.location.pathname;
 
-socket.emit("join", Number(lobby[1]))
+socket.emit("join", Number(lobby[1]));
 
 socket.on("join", (success) => {
     if(success === 1) {
         // you have joined the room, start to check for whether the other person is in, then place ships, then run game
-        console.log("heard back from join with success: " + success)
-        alert("You successfully joined the room")
+        console.log("heard back from join with success: " + success);
+        alert("You successfully joined the room");
     }
     else {
-        console.log("heard back from join with success: " + success)
-        alert("The room is already full.")
+        console.log("heard back from join with success: " + success);
+        alert("The room is already full.");
         // send to an error / index page. do not actually play/send messages
     }
 })
+
+
+for(let i = 0; i < 9; i++) {
+    let tile = document.createElement("div");
+    tile.innerText = "tile";
+    tile.className = "tile";
+    document.getElementById("gameboard").appendChild(tile);
+}
